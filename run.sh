dd34071d1b8bb743f8679dc3f864361b92bf3f29
NX <976180942@qq.com>
2022-12-08 11:52:11 +0800

    test

--- run.sh
+++ run.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-$serviceName = "github-actions-test"
+serviceName = "github-actions-test"
 
 # 停止服务
 systemctl stop $serviceName
 rm main -f
 
 # 重新编译
+export PATH=$PATH:/usr/local/go/bin
 go mod tidy
 go build main.go
 
